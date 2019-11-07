28ebe5522368b6613c4cc8e9743f0781fda33853
ZachyPacky <57198378+ZachyPacky@users.noreply.github.com>
2019-11-06 21:52:56 -0500

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,11 +1,26 @@
-if (loginSuccessful) {
-  logger.severe("User login succeeded for: " + sanitizeUser(username));
-}
-else {
-  logger.severe("User login failed for: " + sanitizeUser(username));
-}
+public class R00_IDS03_J{
+    /*
+    if (loginSuccessful) {
+        logger.severe("User login succeeded for: " + username);
+    }
+    else {
+        logger.severe("User login failed for: " + username);
+    }
+    */
+    
+    
 
-public String sanitizeUser(String username) {
-  return Pattern.matches("[A-Za-z0-9_]+", username))
-    ? username : "unauthorized user";
+    public String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9_]+", username)) 
+            ? username : "unauthorized user";
+    }
+    
+    public static void main(String []args){
+        if (loginSuccessful) {
+            printf("User login succeeded for: " + sanitizeUser(username));
+        }
+        else {
+            printf("User login failed for: " + sanitizeUser(username));
+        }
+    }
 }
