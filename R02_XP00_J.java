b8fdcf02b8895d74d400c50493300d89e3d06bf6
ZachyPacky <57198378+ZachyPacky@users.noreply.github.com>
2019-11-07 21:19:04 -0500

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,4 +1,3 @@
- edit
 import java.io.*;
 public class R02_XP00_J{ //EXP03-J. Do not use the equality operators when comparing values of boxed primitives
     /*
@@ -20,12 +19,4 @@ public class R02_XP00_J{ //EXP03-J. Do not use the equality operators when compa
     public static void main(String []args) {
         deleteFile();
     }
-
-public void deleteFile() {
-  File someFile = new File("someFileName.txt");
-  //Do something with someFile
-  if (!someFile.delete()) {
-    //Handle failure to delete the file
-  }
- master
 }
