092a204630bf6ce9c4b1415ac23bb40260cc2e91
ZachyPacky <57198378+ZachyPacky@users.noreply.github.com>
2019-11-07 21:19:33 -0500

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,4 +1,3 @@
- edit
 import java.io.*;
 import java.util.*;
 public class R03_NUM03_J{ //NUM03-J. Use integer types that can fully represent the possible range of unsigned data
@@ -20,8 +19,4 @@ public class R03_NUM03_J{ //NUM03-J. Use integer types that can fully represent 
 
         getInteger(data);
     }
-
-public static long getInteger(DataInputStream is) throws IOException {
-  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
- master
 }
