c9be3244f0c03ad5355e27676be5bb05dec770a8
ZachyPacky <57198378+ZachyPacky@users.noreply.github.com>
2019-11-07 21:18:15 -0500

    Update R06_MET01_J.java

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,4 +1,3 @@
- edit
 import java.io.*;
 import java.util.*;
 public class R06_MET01_J { //MET01-J. Never use assertions to validate method arguments
@@ -43,16 +42,4 @@ public class R06_MET01_J { //MET01-J. Never use assertions to validate method ar
         a = getAbsAdd(x, y);
         System.out.println("a is " + a);
     }
-
-public static int getAbsAdd(int x, int y) {
-  if (x ==Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
-    throw new IllegalArgumentException();
-  }
-  int absX = Math.abs(x);
-  int absY = Math.abs(y);
-  if (absX > Integer.MAX_VALUE - absY) {
-    throw new IllegalArgumentException();
-  }
-  return absX + absY;
- master
 }
